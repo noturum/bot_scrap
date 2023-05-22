abce9c752844159e90cdfea29d42eb361b989f74
noturum <kolyan.marchenkos@gmail.com>
2023-05-22 08:18:49 +0700

    22
    22

--- parser.py
+++ parser.py
@@ -1,70 +1,174 @@
 import threading
 import time
+from selenium.webdriver.common.keys import Keys
+import os
+from selenium.webdriver.support.select import Select
+from selenium import webdriver
+from selenium.common import exceptions as exc
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.common.by import By
 
 import bs4
 from bs4 import BeautifulSoup as bs
 import requests
-args={'domain':'https://www.avito.ru','attr':[{'data-marker':'item'},{'class':'iva-item-titleStep'}],'tag':'a'}
-class Parser(threading.Thread):
-    PAUSE='pause'
-    STOP='stop'
-    IDLE='idle'
-    RUN='run'
-    ERROR='error'
-    def __init__(self,args,url):
+
+args = {'domain': 'https://www.avito.ru', 'attr': [{'data-marker': 'item'}, {'class': 'iva-item-titleStep'}],
+        'tag': 'a'}
+
+
+class Parser():
+    PAUSE = 'pause'
+    STOP = 'stop'
+    IDLE = 'idle'
+    RUN = 'run'
+    ERROR = 'error'
+
+    def __init__(self, uid=None, args=None):
         super().__init__()
-        self.url=url
-        self.args=args
+        self.tabs = []
+        # self.args=args
         self.stop = threading.Event()
         self.pause = threading.Event()
-        self.state=None
+        self.state = None
+        self.driver = self.getDriver(uid)
+
+    def add_tab(self, url):
+        self.driver.switch_to.new_window('tab')
+        self.tabs.append({'id': self.driver.current_window_handle, 'url': url, 'args': args})
+
+    def parse(self):
+        while True:
+            for tab in self.tabs:
+                self.state = self.RUN
+                tmp = None
+                while not self.stop.isSet():
+                    if self.pause.isSet():
+                        self.pause.wait()
+                    try:
+                        self.driver.switch_to.window(tab['id'])
+                        self.driver.get(tab['url'])
+                        time.sleep(1000)
+
+                        soup = bs(self.driver.page_source, 'lxml')
+                    except Exception as e:
+
+                        pass  # здесь обработку запроса
+                        self.state = self.ERROR
+                        print(e)
+                        time.sleep(100)
+                    for step in args['attr']:
+                        tmp = soup.find(attrs=step)
+                    if 'tag' in args:
+                        link = args['domain'] + tmp.find(args['tag']).get('href')
+                    print(link)
+                    time.sleep(10 / len(self.tabs))
+
+    def getDriver(self, user, headless: bool = True):
+        options = webdriver.ChromeOptions()
+        options.set_capability(
+            "goog:loggingPrefs", {"performance": "ALL"})
+        # options.add_argument('--allow-profiles-outside-user-dir')
+        # options.add_argument('--enable-profile-shortcut-manager')
+        # options.add_argument(r'user-data-dir=.\User')
+
+        # options.add_argument(f'--profile-directory={user}')
+        options.add_argument(
+            '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+        if headless == True:
+            options.add_argument("start-maximized")
+            options.add_argument("disable-infobars")
+            options.add_argument("--disable-extensions")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--no-sandbox")
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+            prefs = {"profile.managed_default_content_settings.images": 2}
+            options.add_experimental_option("prefs", prefs)
+        options.add_argument('log-level=3')
+        caps = DesiredCapabilities().CHROME
+
+        caps["pageLoadStrategy"] = "none"
+        try:
+            driver = webdriver.Chrome('chromedriver.exe', desired_capabilities=caps, options=options)
+        except exc.SessionNotCreatedException as e:
+            print(e)
+            exit(0)
+
+        return driver
 
     def stop(self):
-        self.state=self.STOP
+        self.state = self.STOP
         self.pause.set()
         self.stop.set()
+        self.driver.close()
+        self.driver.quit()
 
     def pause(self):
         if self.state == self.PAUSE:
             self.pause.clear()
-            self.state=self.RUN
+            self.state = self.RUN
 
         else:
             self.pause.set()
-            self.state=self.PAUSE
+            self.state = self.PAUSE
 
     def run(self):
-        self.state=self.RUN
-        tmp=None
-        while not self.stop.isSet():
-            if self.pause.isSet():
-                self.pause.wait()
+        self.add_tab('https://www.avito.ru/novosibirsk/telefony?cd=1')
+        self.parse()
+        # self.state=self.RUN
+        # tmp=None
+        # while not self.stop.isSet():
+        #     if self.pause.isSet():
+        #         self.pause.wait()
+        #     try:
+        #         soup=bs(requests.get(self.url).text, 'lxml')
+        #     except :
+        #         pass#здесь обработку запроса
+        #         self.state=self.ERROR
+        #         return
+        #     for step in self.args['attr']:
+        #         tmp=soup.find(attrs=step)
+        #     if 'tag' in self.args:
+        #         link=self.args['domain']+tmp.find(self.args['tag']).get('href')
+        #     print(link)
+
+import setting
+import requests, json, sys
+
+
+class Avito():
+    def __init__(self, url):
+        self.url = url
+        self.s=requests.session()
+        self.params = self._set_param()
+    def _set_param(self):
+        if self.url.find('m.') != -1:
+            url = self.url
+        else:
+            url = self.url.replace('www', 'm')
+        d=Parser().getDriver('12')
+        d.get(url)
+        time.sleep(3)
+        logs_raw = d.get_log("performance")
+        logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
+        for log in logs:
             try:
-                soup=bs(requests.get(self.url).text, 'lxml')
-            except :
-                pass#здесь обработку запроса
-                self.state=self.ERROR
-                return
-            for step in self.args['attr']:
-                tmp=soup.find(attrs=step)
-            if 'tag' in self.args:
-                link=self.args['domain']+tmp.find(self.args['tag']).get('href')
-            print(link)
-while True:
-    headers = {
-        'cookie':'MltIuegZN2COuSe=EOFGWsm50bhh17prLqaIgdir1V0kgrvN; u=2xuq75rk.1gc8h8g.1v42xu1s6qy00; v=1681998400; buyer_laas_location=644560; luri=vladivostok; buyer_location_id=644560; abp=1; _gcl_au=1.1.1991880787.1681998418; _ga=GA1.1.2117973978.1681998419; tmr_lvid=652e30cf112e543783cf4dcc07270a5b; tmr_lvidTS=1681998418738; _ym_uid=1681998420246812276; _ym_d=1681998420; _ym_isad=1; f=5.91b0cc4c87cec2ec4b5abdd419952845a68643d4d8df96e9a68643d4d8df96e9a68643d4d8df96e9a68643d4d8df96e94f9572e6986d0c624f9572e6986d0c624f9572e6986d0c62ba029cd346349f36c1e8912fd5a48d02c1e8912fd5a48d0246b8ae4e81acb9fa143114829cf33ca746b8ae4e81acb9fa46b8ae4e81acb9fae992ad2cc54b8aa8b175a5db148b56e9bcc8809df8ce07f640e3fb81381f359178ba5f931b08c66a59b49948619279110df103df0c26013a2ebf3cb6fd35a0ac2da10fb74cac1eab268a7bf63aa148d2dc5322845a0cba1aba0ac8037e2b74f92da10fb74cac1eab71e7cb57bbcb8e0f2da10fb74cac1eab2da10fb74cac1eab0df103df0c26013a037e1fbb3ea05095de87ad3b397f946b4c41e97fe93686adecb8388123cde3fbe04c1cd198727a0602c730c0109b9fbbc60ec9d2f66a8631c9fbdd7f5877c6d729aa4cecca288d6bf861d1d271434c50b71a88e8e84832962ebf3cb6fd35a0ac0df103df0c26013a28a353c4323c7a3a140a384acbddd748f37f1e40e74776ec3de19da9ed218fe23de19da9ed218fe2ddb881eef125a8703b2a42e40573ac3c8edd6a0f40cbfd87da3d420d6cca468c; uxs_uid=d43dd270-df81-11ed-8519-b57c5ae0b4f6; ft="9k17cz++evIFwBVcOi8b50BFOOsZB7blUiG7ni6mJGhXeZ4DShpfGH0KEYZZ/YFKuD80Vf/GId4USpOQc2LSZAbroQKltxXMDLKkioqcvRS7sC/J6+ONAeibJN9JxjXGoO5CGw7eLhw7VjDuKhmhMg0XusJf/Dheu/E1lLdhZezDK5PDZbvcjCZ8eLdLLiL3"; dfp_group=8; tmr_detect=0%7C1682000291914; _ym_visorc=b; sx=H4sIAAAAAAAC%2F1TMTW6rMBAH8Lt4zcKff3u4je0Zv7wSUhDgBEXcvatI7QV%2BbwUAlSMagQI8SGIRRxyDrjUyqfGtuhqVv93uvf%2FLW4J%2FyOTuOe5u3stEeV3nUw1K1GiQrE4%2BaX0NKgnrGtFa0dlyqiyFTDW2aknBFPORndDmUlt2NsjHwS3Ko5zdyTp90Wv9LQPBX4MqrgC6xGSz1r5KlcihkgX7UCDykfHsyxwX2U7LvS3B0v58nf9BtCb3ffyRCem6fgIAAP%2F%2FFvTq7w8BAAA%3D; _ga_M29JC28873=GS1.1.1681998418.1.1.1682000570.60.0.0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
-
-    response = requests.get('https://www.avito.ru/vladivostok/bytovaya_elektronika', headers=headers, timeout=10)
-    soup = bs(response.text, 'lxml')
-    print(soup.text)
-    for step in args['attr']:
-        tmp=soup.find(attrs=step)
-    if 'tag' in args:
-        link=args['domain']+tmp.find(args['tag']).get('href')
-    print(link)
-    time.sleep(10)
+                if log['params']['request']['url'].find('/mav?')!=-1:
+                    self.params=dict(log['params']['request']['postData'])
+            except:
+                continue
+    def parse(self):
+        res = self.s.get(setting.api, params=self.params)
+        a=res.text
+        print(res.text)
 
 
 
 
+
+#Avito('https://www.avito.ru/novosibirsk/telefony/mobile-ASgBAgICAUSwwQ2I_Dc').parse()
+from fp.fp import FreeProxy
+print(FreeProxy(https=True).get())
+#requests.session().get('https://2ip.ru/',proxies={})
--- proxy.py
+++ proxy.py
@@ -1,41 +1,50 @@
+import random
 
 from bs4 import BeautifulSoup
 import requests
 from requests import exceptions as exc
-import dbConnect as db
-t=[]
-
-def fillproxy():
-    proxy=[]
-    proxies = {
-        "http": "http://marchenko_ns:Yjz,hm2021@nov_proxy.mfnso.local:8080/",
-        "https": "http://marchenko_ns:Yjz,hm2021@nov_proxy.mfnso.local:8080/"
-    }
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
-    try:
-        soup=BeautifulSoup(requests.get(url='https://free-proxy-list.net/',headers=HEADERS,proxies=proxies).text, 'lxml')
-    except exc.RequestException as e:
-        print('Проблемы, Хьюстон!(реквесты не идут)')
-        print(e)
-        return None
-    elements=soup.find_all(attrs={'class':'table table-striped table-bordered'})
-    for el in elements:
-        for a in el.find('tbody').find_all('tr'):
-            proxy.append((a.find_all('td')[0].text,a.find_all('td')[1].text))
-            if len(proxy)==20:
-                return proxy
-def checkproxy(proxy):
-    for p in proxy:
+class Proxy():
+    def __init__(self):
+        self.lenth = 20
+        self.proxy=self.fillproxy()
+    def fillproxy(self):
+        proxy = []
+        tmp=[]
+        HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
         try:
-            requests.get("https://www.google.com/", proxies={"http": "http://{}:{}".format(p[0],p[1]) }, timeout=3)
-        except Exception as x:
-            proxy.pop(proxy.index(p))
+            soup = BeautifulSoup(requests.get(url='https://free-proxy-list.net/', headers=HEADERS).text, 'lxml')
 
-        if len(db.executeSql('select * from proxy where url="{}" and port="{}"'.format(p[0],p[1]))) >0 :
-            continue
-        else:
-            db.executeSql('insert into proxy(url,port) values("{}","{}")'.format(p[0],p[1]),True)
-def getproxy(id):
-    p=db.executeSql('select * from proxy where id={}"'.format(id))
-    return {"http": "http://{}:{}".format(p[1],p[2]) }
+
+        except exc.RequestException as e:
+            print('Проблемы, Хьюстон!(реквесты не идут)')
+            print(e)
+            return None
+        elements = soup.find_all(attrs={'class': 'table table-striped table-bordered'})
+        for el in elements:
+            for a in el.find('tbody').find_all('tr'):
+
+                if len(proxy) == self.lenth:
+                    break
+                if a.find_all('td')[4].text=='elite proxy':
+                    proxy.append((a.find_all('td')[0].text, a.find_all('td')[1].text))
+
+
+        for p in proxy:
+
+            try:
+                a=requests.get("http://icanhazip.com", proxies={"http": f'{p[0]}:{p[1]}',"https": f'{p[0]}:{p[1]}'}, timeout=2)
+            except Exception as x:
+                continue
+            else:
+                print(a.text)
+                tmp.append({"http": f'{p[0]}:{p[1]}',"https": f'{p[0]}:{p[1]}'})
+        return tmp
+
+
+
+
+
+
+
 
